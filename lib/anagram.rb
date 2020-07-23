bf31ecb13a30c7c56ac2135ac721ba5008be8f9f
Cristian Deleon <libaration@gmail.com>
2020-07-23 01:05:43 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :startingword
+
+    def initialize(startingword)
+        @startingword = startingword
+    end
+
+    def match(wordlist)
+      matchingwords = []
+        wordlist.collect do |word|
+            
+            if word.split("").sort == @startingword.split("").sort
+                matchingwords << word
+            end
+        end
+        matchingwords
+    end
+end
