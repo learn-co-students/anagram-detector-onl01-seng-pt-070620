594ef34a1292a2f61366db3bde3d7d89f715da15
Katie Le <lethikatie@gmail.com>
2020-07-28 02:17:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,8 @@ def initialize(word)
  @word = word 
 end 
 
-  def match(array_of_words)
-    array_of_words.select do |x|
+  def match(words_array)
+    words_array.select do |x|
       (@word.split("").sort) == (x.split("").sort)
     end
   end
