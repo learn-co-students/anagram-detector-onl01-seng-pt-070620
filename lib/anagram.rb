34a784034e8721585771d4bb9475ac885fa31178
Sean Ransonette <sransonette@gmail.com>
2020-07-29 01:24:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :anagram 
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(words)
+    new_array = [] 
+    words.each do |word|
+    letters = word.split(//)
+    anagram_letters = @anagram.split(//)
+    if letters.sort == anagram_letters.sort
+      new_array << word
+    end
+  end
+  new_array
+end
+end
+
