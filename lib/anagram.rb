5b64ab7cb312dbfaa45e599e9bf0665b28ef6f96
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-30 21:16:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram 
+  anagrams=[]
+  
+  def initialize(word)
+    @word= word 
+  end
+  
+  def match(anagram_array)
+    anagram_array.select { |word| word.split("").sort == @word.split("").sort} 
+  end 
+end 
