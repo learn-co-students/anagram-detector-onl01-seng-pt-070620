cd337e7de449272c4741e8b5055c055359d28911
Jill Short <jillian.b.short@gmail.com>
2020-08-01 00:36:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(words)
+    words.each do |word|
+      if word.split("").sort == @word.split("").sort
+        return @word.split("").sort
+      else
+        return []
+      end
+    end
+  end
+  
+end
