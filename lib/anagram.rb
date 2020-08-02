adc40f0a35130deca675cc5eaad6bf6d49e8d3a2
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-02 18:36:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram 
+
+def initialize(word)
+ @word=word
+ end
+
+def match(possible_anagrams)
+  possible_anagrams.select do |anagram|
+    @word.split("").sort == anagram.split("").sort
+    
+  end
+end
+
+
+end
