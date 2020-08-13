10bf23da9d22cb0dff96655512d78679d5d8fac4
Cory Marker <cmarker4@gmail.com>
2020-08-13 02:49:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_arr)
+    word_arr.select do |e|
+      @word.split("").sort == e.split("").sort
+    end
+  end
+end
